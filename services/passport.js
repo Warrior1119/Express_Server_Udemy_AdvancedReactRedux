a533cff56c1960c6419cea7545cbe907fa23a248
Warrior1119 <warriorjhs@outlook.com>
2020-03-18 21:18:58 -1000

    add strategy with passport

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,10 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader("authorization"),
+  secretOrKey: config.secret
+};
 
 // Create JWT strategy
 const jwtlogin = new JwtStrategy(jwtOptions, function(payload, done) {
@@ -26,3 +29,4 @@ const jwtlogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell passport to use this strategy
+passport.use(jwtlogin);
