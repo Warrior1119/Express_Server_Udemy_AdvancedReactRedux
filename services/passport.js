fd666bc729dd2008e4e273bdb0ace232e16f164f
Warrior1119 <warriorjhs@outlook.com>
2020-03-19 06:59:58 -1000

    add signin/complete backend

--- services/passport.js
+++ services/passport.js
@@ -24,6 +24,16 @@ const localLogin = new LocalStrategy(localOptions, function(
     }
 
     // compare password - is `password` equal to user.password
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
   });
 });
 
@@ -53,3 +63,4 @@ const jwtlogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtlogin);
+passport.use(localLogin);
