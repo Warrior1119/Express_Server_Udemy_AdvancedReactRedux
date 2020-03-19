94d001d04b1e20f31eb8e70b2688950ca5019aba
Warrior1119 <warriorjhs@outlook.com>
2020-03-18 21:38:25 -1000

    add purpose of local strategy

--- services/passport.js
+++ services/passport.js
@@ -12,9 +12,17 @@ const localLogin = new LocalStrategy(localOptions, function(
   password,
   done
 ) {
-  // Verify this username and password, call done with the user
-  // if it is the correct username and password
+  // Verify this email and password, call done with the user
+  // if it is the correct email and password
   // otherwise, call done with false
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+  });
 });
 
 // Setup options for JWT Strategy
